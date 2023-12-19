0d36916b7b21838bcb76e5f908514e7996994aaa
shagun-nickelfox <shagun.bansal@nickelfox.com>
2023-12-19 20:04:17 +0530

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Nickelfox on 19/12/23.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
